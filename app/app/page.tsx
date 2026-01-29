eb4e5c3e263fce1ee7862f11af1c3f3cbf186669
annagrimaldi92a-sudo <anna.grimaldi92a@gmail.com>
2026-01-29 21:51:23 +0000

    Redirect / to /new

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,8 +1,5 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>CareBalance-N</h1>
-      <p>Home OK</p>
-    </main>
-  );
+import { redirect } from "next/navigation";
+
+export default function Home() {
+  redirect("/new");
 }
